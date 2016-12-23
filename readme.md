db6817573152b6e4219032fcdc3f21b469ac99b5
boboworld427 <boboworld427@gmail.com>
2016-12-22 23:50:47 -0600

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,73 @@
+Readme {.title .toc-ignore}
+======
+
+#### *December 22, 2016* {.date}
+
+This is for the project of Getting and Cleaning Data. The link to the
+Github Repo is:
+
+There are 3 files :
+
+\*readme.md
+
+\*run\_analysis.R
+
+\*codebook.md : This file goes through the steps in run\_analysis.R. At
+the end, it shows the list of variables from the tidydata.
+
+\*tidydata.txt: The tidy data set from codebook.md
+
+#### A brief look at the data
+
+The name of the data is Human Activity Recognition Using Smartphones
+Dataset, which is provided in:
+[http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+The data was resulted from an experiment using a smartphone to measure
+some movements when people walk,sit or lay down, etc. There were 30
+subjects, which were divided into groups of test and training. There
+were 6 activities these subject perform which are walking, walking
+downstairs, walking upstairs, sitting, standing and laying. When the
+subject were performing such activities, accelerometer and gyroscope
+inside the smartphones measure the motions. For example, body
+acceleration in 3 dimensions,x,y,and z, were measure or body jerking
+magnitude using gyroscope were measure. For each these measurements,
+quantities like mean,standard deviation,maximum or angle were estimated
+and make up the columns of the original data set. Please refer to
+feature.txt in the original dataset for more information.
+
+#### Problems
+
+The zipped file contained multiple datasets which had to be combined.
+The data originally had over 500+ columns and we were not interested in
+all of them. Also, the name of the columns were messy and ambiguous. The
+data didn’t have any missing values.
+
+#### Fix
+
+\*We added and changed the variable names. We changed the abbreviated
+words to full words so that the variable names become more intuitive.
+Also we removed chracters like () and – because they were making the
+name very messy.We used the r function grep for this.
+
+-t \\(\\rightarrow\\) time\
+ -f \\(\\rightarrow\\) frequency\
+ -Acc \\(\\rightarrow\\) Accelerometer\
+ -Gyro \\(\\rightarrow\\) Gyroscope\
+ -Mag \\(\\rightarrow\\) Magnitude\
+ -BodyBody \\(\\rightarrow\\) Body
+
+\*Also, We add eliminated all the features that are not from mean or
+standard deviation measurements.
+
+\*We added a new variable called “group” to indicate if a subject is
+from the test or training group.
+
+\*We melted the mean and standard deviation and made a new column out of
+it, named “measurement”, which reduced the number of columns.
+
+\*We reduced the number of columns 563 \\(\\rightarrow\\) 37.
+
+\*We rearranged the order of columns so the identity variables come
+first.
+
+\*We rearranged the order of rows according to the subject number(1-30).
---
+++ run_analysis.R
@@ -0,0 +1,85 @@
+
+feature<-read.table("features.txt",header = F)
+activity_labels<-read.table("activity_labels.txt")
+
+X_test<-read.table("X_test.txt",header=F)
+Y_test<-read.table("y_test.txt",header=F)
+subject_test<-read.table("subject_test.txt")
+
+X_train<-read.table("X_train.txt",header=F)
+Y_train<-read.table("y_train.txt",header=F)
+subject_train<-read.table("subject_train.txt")
+
+
+names(subject_test)<-"ID"
+names(subject_train)<-"ID"
+
+names(Y_test)<-"activity"
+names(Y_train)<-"activity"
+Y_test$activity<-factor(Y_test$activity,labels=activity_labels$V2)  #change the numeric values to 
+Y_train$activity<-factor(Y_train$activity,labels=activity_labels$V2)#factors and label the factors.
+
+
+feature.names<-as.character(feature$V2)
+names(X_test)<-feature.names 
+names(X_train)<-feature.names
+
+
+MeanAndStd<-grep("[Mm][Ee][Aa][Nn]|[Ss][Tt][Dd]",names(X_test))
+X_test<-X_test[,MeanAndStd]                
+X_train<-X_train[,MeanAndStd]
+meanFreqAngle<-grep("[Mm][Ee][Aa][Nn][Ff][Rr][Ee][Qq]|[Aa][Nn][Gg][Ll][Ee]",names(X_test))
+X_test<-X_test[,-meanFreqAngle]
+X_train<-X_train[,-meanFreqAngle]
+names(X_test)<-gsub("\\()|-","",names(X_test))
+names(X_train)<-gsub("\\()|-","",names(X_train))
+
+X_test<-cbind(subject_test,Y_test,X_test)    #combine with ID and activity columns
+X_train<-cbind(subject_train,Y_train,X_train)
+
+X_test<-cbind(group=factor("test"),X_test)   
+X_train<-cbind(group=factor("training"),X_train)
+
+
+library(dplyr)
+
+X_test<-tbl_df(X_test)
+X_train<-tbl_df(X_train)
+
+fulltable<-rbind(X_test,X_train)
+
+
+fulltable<-fulltable %>% select(ID,activity,group,grep("[Mm][Ee][Aa][Nn]",names(fulltable)),grep("[Ss][Tt][Dd]",names(fulltable)))%>%arrange(ID,activity,group)
+#Selecting only he measure from mean and std
+
+
+meantable<-fulltable %>% select(ID:group,grep("[Mm][Ee][Aa][Nn]",names(fulltable))) %>% mutate(measure=factor("mean")) 
+stdtable<-fulltable %>%select(ID:group,grep("[Ss][Tt][Dd]",names(fulltable))) %>% mutate(measure=factor("standardDeviation"))
+
+meantable<-select(meantable,ID:group,measure,grep("[Mm][Ee][Aa][Nn]",names(meantable))) 
+stdtable<-select(stdtable,ID:group,measure,grep("[Ss][Tt][Dd]",names(stdtable)))
+
+names(meantable)<-gsub("mean","",names(meantable))
+names(stdtable)<-gsub("std","",names(stdtable))
+
+newtable<-rbind(meantable,stdtable)
+newtable<-select(newtable,ID,group,activity,measure,5:37)
+
+
+names(newtable)<-gsub("^t","time",names(newtable))
+names(newtable)<-gsub("^f","freqency",names(newtable))
+names(newtable)<-gsub("[Bb][Oo][Dd][Yy][Bb][Oo][Dd][Yy]","Body",names(newtable))
+names(newtable)<-gsub("[Aa][Cc][Cc]","Accelerometer",names(newtable))
+names(newtable)<-gsub("[Gg][Yy][Rr][Oo]","Gyroscope",names(newtable))
+names(newtable)<-gsub("[Mm][Aa][Gg]","Magnitute",names(newtable))
+
+
+dim(newtable)
+str(newtable)
+
+newtable2<-aggregate(. ~ID+group+activity+measure, newtable, mean)
+newtable2<-arrange(newtable2,ID)  
+
+View(newtable2)
+
+#write.table(newtable2, "c:/tidydata.txt", sep="\t")
